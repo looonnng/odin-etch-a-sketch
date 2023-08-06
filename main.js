146ebb47868aed52e23b529a91b10f766f5f8246
T14s <122837733+looonnng@users.noreply.github.com>
2023-08-06 15:30:08 -0500

    Rewrote createSquare to take in user input Added Reset button to erase whole sketch

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 const container = document.querySelector(".container");
+const createSquare = (numOfSquare) => {
+  const squareLength = 41.75 / numOfSquare - 0.25; // squareLength = (containerLength / numOfSquare) - gap
 
-const createSquare = () => {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < numOfSquare ** 2; i++) {
     const square = document.createElement("div");
+    square.style.width = `${squareLength}rem`;
+    square.style.height = `${squareLength}rem`;
     square.className = "square";
     container.appendChild(square);
   }
 };
+createSquare(5);
 
-createSquare(); // Create 4 x 4 grid
+//Color when hover
 const squares = document.querySelectorAll(".square");
-
-console.log(squares);
-
 function hover(squares, color) {
   squares.forEach((square) =>
     square.addEventListener("mouseenter", (e) => square.classList.add(color))
   );
 }
+hover(squares, "color");
 
-hover(squares, "red");
+//Reset button
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+  squares.forEach((square) => 
+    square.classList.remove("color")
+  );
+});
+
+//User input to resize grid
